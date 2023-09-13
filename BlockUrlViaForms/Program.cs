cbc3d0d19071759133b10a4eca705d53edd20594
Elanchezhiyan-AIT <elanchezhiyan.p@aitechindia.com>
2023-09-13 17:07:25 +0530

    Removed dateTime in program.cs

--- BlockUrlViaForms/Program.cs
+++ BlockUrlViaForms/Program.cs
@@ -16,7 +16,6 @@ namespace BlockUrlViaForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BlockURL());
-            var test = DateTime.Now;
         }
     }
 }
